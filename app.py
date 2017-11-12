20e8b48fb87fb3a3fcee4694662671b63804b5b0
anish2454 <shenoy.anish.227@gmail.com>
2017-11-12 13:31:27 -0500

    Added files

--- app.py
+++ app.py
@@ -10,23 +10,27 @@ import json
 import random
 
 app = Flask(__name__)
-api_key = "E79xFj7MoBIAJ6CvVW50Q9VNrNxjrs2n5nLRRrpy"
+nasa_api_key = "E79xFj7MoBIAJ6CvVW50Q9VNrNxjrs2n5nLRRrpy"
+nba_api_key = "5010b3883a104877bb7ab92cb6daa7f3"
 
-def getDict(url):
-    uresp = urllib2.urlopen(url)
+def getData(url):
+    temp = urllib2.Request(url, headers={'Ocp-Apim-Subscription-Key': nba_api_key})
+    uresp = urllib2.urlopen(temp)
     #print uresp
     d = json.loads(uresp.read())
     #print d
+    uresp.close()
     return d
 
 @app.route("/")
 def home():
-    url1 = "https://api.nasa.gov/planetary/apod?api_key=" + api_key
-    url2 = "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=2015-6-3&api_key=" + api_key
-    d1 = getDict(url1)
-    d2 = getDict(url2)
-    print d1
-    print d2
+
+    url1 = "https://api.nasa.gov/planetary/apod?api_key=" + nasa_api_key
+    url2 = "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=2015-6-3&api_key=" + nasa_api_key
+    d1 = getData(url1)
+    d2 = getData(url2)
+    #print d1
+    #print d2
     title1 = d1['title']
     src1 = d1['url']
     desc1 = d1['explanation']
@@ -35,7 +39,20 @@ def home():
     #print d2['photos'][rand_int]
     src2 = d2['photos'][rand_int]['img_src']
     desc2 = "Taken On: " + d2['photos'][rand_int]['earth_date']
-    return render_template("template.html", title1 = title1, src1 = src1, desc1 = desc1, title2 = title2, src2 = src2, desc2 = desc2)
+
+
+    url_nba_team = "https://api.fantasydata.net/v3/nba/scores/JSON/AllTeams"
+    teams = getData(url_nba_team)
+    rand_int2 = random.randint(0, len(teams))
+    team = teams[rand_int2]
+    src3 = team.pop("WikipediaLogoUrl")
+    url_nba_players = "https://api.fantasydata.net/v3/nba/stats/JSON/PlayerSeasonStats/2017"
+    players = getData(url_nba_players)
+    rand_int3 = random.randint(0, len(players))
+    player = players[rand_int3]
+    return render_template("template.html", title1 = title1, src1 = src1, desc1 = desc1, title2 = title2, src2 = src2, desc2 = desc2, team = team, src3 = src3, player = player)
+
+
 
 if __name__ == '__main__':
     app.debug = True
